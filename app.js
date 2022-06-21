901fed00019a4b4e0ba6f5422c8e0af1dff02313
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2022-06-21 20:27:38 -0300

    Conectando ao banco de dados

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
 var app = require('./config/server')
 
-app.get('/contato',function(req,res){
-    res.render('site/contato');
-});
-
-app.get('/',function(req,res){
-    res.render('site/home');
-});
-
-
-app.listen(8000,function(){
-    console.log("localhost:8000");
-});
+app();
