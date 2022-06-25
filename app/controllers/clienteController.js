3c72d0df584a7b5d773e854b307e0ff24c2b3172
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2022-06-25 03:04:23 -0300

    Validando dados

--- app/controllers/clienteController.js
+++ app/controllers/clienteController.js
@@ -2,13 +2,29 @@ var clienteModel = require('../models/clienteModel')();
 
 module.exports.index = function(req,res){
     clienteModel.all(function(erro, resultado){
-        res.render('site/home',{clientes:resultado});
+        res.render('site/home',{clientes:resultado,erros:{},dados:{}});
     });
 };
 
 module.exports.store = function(req,res){
 
     var dados = req.body;
+
+    req.assert('nome','!!! Preencha um Nome').notEmpty();
+    req.assert('email','!!! Preencha um E-mail').notEmpty();
+    req.assert('email','!!! Preencha um E-mail válido').isEmail();
+    req.assert('nome','!!! Nome deve ter de 3 a 20 caracteres').len(3,20);
+
+
+    var validacaoErros = req.validationErrors();
+
+    if(validacaoErros){
+        console.log(validacaoErros);
+        clienteModel.all(function(erro, resultado){
+            res.render('site/home',{clientes:resultado,erros:validacaoErros,dados:dados});
+        });
+        return;
+    }
 
     clienteModel.save(dados,function(erro, resultado){
         if(!erro){
--- config/server.js
+++ config/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 module.exports = function(){
     var express = require('express');
     var bodyParser = require('body-parser');
+    var expressValidator = require('express-validator');
 
     var app = express();
     app.set('view engine', 'ejs');
@@ -11,6 +12,8 @@ module.exports = function(){
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
+
+    app.use(expressValidator());
     
     var rotas = require('../app/routes/web');
     rotas(app);
